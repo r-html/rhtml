350283b59380caa92858dbf26a693cb0313cc653
Kristiyan Tachev <kristiqn.tachev@gmail.com>
2020-03-08 15:04:50 +0000

    0.0.16

--- packages/graphql/settings.d.ts
+++
@@ -1,8 +0,0 @@
-import { GraphqlModuleConfig } from '@rxdi/graphql-client';
-import { DEFAULTS } from './tokens';
-export interface GraphqlSettings {
-    config: GraphqlModuleConfig;
-    defaults: DEFAULTS;
-}
-export declare let GraphqlSettings: GraphqlSettings;
-export declare function setConfig(config: GraphqlSettings): GraphqlSettings;
--- packages/graphql/tokens.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const core_1 = require("@rxdi/core");
-exports.DEFAULTS = new core_1.InjectionToken();
-//# sourceMappingURL=tokens.js.map
