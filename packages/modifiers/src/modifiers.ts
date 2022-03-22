5234374af3d053daa2c5d9b9072fd86f8d315d9e
Kristiyan Tachev <kristiqn.tachev@gmail.com>
2022-03-22 13:37:21 +0200

    feat(modifiers): added observable attributes

--- packages/modifiers/src/modifiers.ts
+++ packages/modifiers/src/modifiers.ts
@@ -15,6 +15,14 @@ export type CrossAxis =
   | 'space-between'
   | 'space-around'
   | 'baseline';
+
+export enum Attributes {
+  FxLayout = 'fxLayout',
+  FxFlex = 'fxFlex',
+  FxFlexFill = 'fxFlexFill',
+  FxLayoutAlign = 'fxLayoutAlign',
+  FxLayoutGap = 'fxLayoutGap'
+}
 /* TODO we need to update to typescript 4 */
 // export type FxLayoutAlign = `${MainAxis} ${CrossAxis}`;
 
@@ -22,66 +30,84 @@ export const isAttribute = (attr: string): boolean | string =>
   typeof attr === 'string' && (attr || attr === '');
 
 export const setFxLayoutAlign = (element: HTMLElement) => {
-  return (fxLayoutAlign = ''): void => {
+  const fxLayoutAlign = element.getAttribute(Attributes.FxLayoutAlign);
+  if (isAttribute(fxLayoutAlign)) {
     const [mainAxis, crossAxis] = fxLayoutAlign.split(' ');
     element.style['place-content'] = `${crossAxis} ${mainAxis}`;
     element.style['align-items'] = crossAxis;
-    element.style['display'] = 'flex';
-  };
+  }
+  element.style['display'] = 'flex';
 };
 
 export const setChildrensFlexFill = (div: HTMLElement): void => {
-  div.style['margin'] = '0';
-  div.style['width'] = '100%';
-  div.style['height'] = '100%';
-  div.style['min-width'] = '100%';
-  div.style['min-height	'] = '100%';
+  const fxFlexFill = div.getAttribute(Attributes.FxFlexFill);
+  if (isAttribute(fxFlexFill)) {
+    div.style['margin'] = '0';
+    div.style['width'] = '100%';
+    div.style['height'] = '100%';
+    div.style['min-width'] = '100%';
+    div.style['min-height	'] = '100%';
+  }
 };
 
 export const setChildrensFlex = (div: HTMLElement) => {
-  return (fxFlex: string): void => {
+  const fxFlex = div.getAttribute(Attributes.FxFlex);
+  if (isAttribute(fxFlex)) {
     div.style['flex'] = '1 1 100%';
     div.style['box-sizing'] = 'border-box';
     if (fxFlex) {
       div.style['max-width'] = fxFlex;
     }
-  };
+  }
+};
+
+export const subscribeToAttributeChanges = (name: string) => (
+  fn: (element: HTMLElement) => void
+) => (element: HTMLElement) => {
+  fn(element);
+  return new MutationObserver(() => fn(element)).observe(element, {
+    attributeFilter: [name.toLocaleLowerCase()],
+    attributes: true
+  });
 };
 
 export const setFxLayout = (element: HTMLElement) => {
-  return (fxLayout = 'row'): void => {
-    // element.style['flex-direction'] = fxLayout;
-    const splitted = fxLayout.split(' ');
+  const layout = element.getAttribute(Attributes.FxLayout);
+  if (isAttribute(layout)) {
+    const splitted = layout.split(' ');
     const [mainAxis, crossAxis] = splitted;
-
-    element.style['box-sizing'] = 'flex';
-    element.style['display'] = 'flex';
     element.style['flex-flow'] =
       splitted.length > 1 ? `${mainAxis} ${crossAxis}` : mainAxis;
-  };
+    element.style['box-sizing'] = 'flex';
+    element.style['display'] = 'flex';
+  }
 };
 
 export const recursion = (div: HTMLElement): void => {
-  const fxFlex = div.getAttribute('fxFlex');
-  const fxFlexFill = div.getAttribute('fxFlexFill');
-  const fxLayout = div.getAttribute('fxLayout');
-  const fxLayoutAlign = div.getAttribute('fxLayoutAlign');
-  const fxLayoutGap = div.getAttribute('fxLayoutGap');
+  const fxFlex = div.getAttribute(Attributes.FxFlex);
+  const fxFlexFill = div.getAttribute(Attributes.FxFlexFill);
+  const fxLayout = div.getAttribute(Attributes.FxLayout);
+  const fxLayoutAlign = div.getAttribute(Attributes.FxLayoutAlign);
+  const fxLayoutGap = div.getAttribute(Attributes.FxLayoutGap);
 
   if (isAttribute(fxFlex)) {
-    setChildrensFlex(div)(fxFlex);
+    subscribeToAttributeChanges(Attributes.FxFlex)(setChildrensFlex)(div);
   }
 
   if (isAttribute(fxFlexFill)) {
-    setChildrensFlexFill(div);
+    subscribeToAttributeChanges(Attributes.FxFlexFill)(setChildrensFlexFill)(
+      div
+    );
   }
 
   if (isAttribute(fxLayout)) {
-    setFxLayout(div)(fxLayout);
+    subscribeToAttributeChanges(Attributes.FxLayout)(setFxLayout)(div);
   }
 
   if (isAttribute(fxLayoutAlign)) {
-    setFxLayoutAlign(div)(fxLayoutAlign);
+    subscribeToAttributeChanges(Attributes.FxLayoutAlign)(setFxLayoutAlign)(
+      div
+    );
   }
 
   if (isAttribute(fxLayoutGap)) {
