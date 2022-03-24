d6c5e459c3a1af766164b542c5fb1795c9158c3c
Kristiyan Tachev <kristiqn.tachev@gmail.com>
2022-03-24 13:24:34 +0200

    fix(modifiers): when using async directive modifiers are not updated fixed with mutation observer

--- packages/modifiers/src/layout/index.ts
+++ packages/modifiers/src/layout/index.ts
@@ -2,11 +2,13 @@ import {
   Component,
   html,
   LitElement,
+  OnDestroy,
+  OnUpdate,
   property,
   TemplateResult
 } from '@rxdi/lit-html';
 
-import { recursion } from './modifiers';
+import { Attributes, recursion } from './modifiers';
 
 @Component({
   selector: 'flex-layout',
@@ -16,15 +18,50 @@ import { recursion } from './modifiers';
     `;
   }
 })
-export class FlexLayout extends LitElement {
+export class FlexLayout extends LitElement implements OnUpdate, OnDestroy {
   @property()
   child: LitElement;
 
+  private observables: MutationObserver[] = [];
+
   OnUpdate(): void {
-    const slot = this.shadowRoot.querySelector('slot');
-    for (const div of [...slot?.assignedElements()]) {
-      recursion.call(this.child, div as never);
+    this.updateAttributes();
+  }
+
+  OnDestroy() {
+    this.unsubscribe();
+  }
+
+  private unsubscribe() {
+    for (const observable of this.observables) {
+      observable.disconnect();
     }
+    this.observables = [];
+  }
+
+  private updateAttributes() {
+    const slot = this.shadowRoot.querySelector('slot');
+    for (const element of [...slot?.assignedElements()]) {
+      if (this.isFxLayout(element)) {
+        this.unsubscribe();
+        this.subscribe(element);
+      }
+      recursion.call(this.child, element);
+    }
+  }
+
+  private isFxLayout(element: Element) {
+    return element.getAttribute(Attributes.FxLayout.toLowerCase());
+  }
+
+  private subscribe(element: Element) {
+    this.unsubscribe();
+    const observer = new MutationObserver(() => this.updateAttributes());
+    observer.observe(element, {
+      subtree: true,
+      childList: true
+    });
+    this.observables.push(observer);
   }
 
   public static modifier(template: TemplateResult): TemplateResult {
