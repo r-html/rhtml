2b1a9f7b7b76604846d5ef117ddb656902fa504c
Kristiyan Tachev <kristiqn.tachev@gmail.com>
2022-03-24 14:24:46 +0200

    fix(modifiers): fixed recursive dom subscription causing memory leaks

--- packages/modifiers/src/layout/index.ts
+++ packages/modifiers/src/layout/index.ts
@@ -2,13 +2,11 @@ import {
   Component,
   html,
   LitElement,
-  OnDestroy,
-  OnUpdate,
   property,
   TemplateResult
 } from '@rxdi/lit-html';
 
-import { Attributes, recursion } from './modifiers';
+import { recursion } from './modifiers';
 
 @Component({
   selector: 'flex-layout',
@@ -18,50 +16,20 @@ import { Attributes, recursion } from './modifiers';
     `;
   }
 })
-export class FlexLayout extends LitElement implements OnUpdate, OnDestroy {
+export class FlexLayout extends LitElement {
   @property()
   child: LitElement;
-
-  private observables: MutationObserver[] = [];
 
   OnUpdate(): void {
     this.updateAttributes();
   }
 
-  OnDestroy() {
-    this.unsubscribe();
-  }
-
-  private unsubscribe() {
-    for (const observable of this.observables) {
-      observable.disconnect();
-    }
-    this.observables = [];
-  }
-
   private updateAttributes() {
     const slot = this.shadowRoot.querySelector('slot');
-    for (const element of [...slot?.assignedElements()]) {
-      if (this.isFxLayout(element)) {
-        this.unsubscribe();
-        this.subscribe(element);
-      }
+    const elements = slot?.assignedElements() ?? [];
+    for (const element of elements) {
       recursion.call(this.child, element);
     }
-  }
-
-  private isFxLayout(element: Element) {
-    return element.getAttribute(Attributes.FxLayout.toLowerCase());
-  }
-
-  private subscribe(element: Element) {
-    this.unsubscribe();
-    const observer = new MutationObserver(() => this.updateAttributes());
-    observer.observe(element, {
-      subtree: true,
-      childList: true
-    });
-    this.observables.push(observer);
   }
 
   public static modifier(template: TemplateResult): TemplateResult {
