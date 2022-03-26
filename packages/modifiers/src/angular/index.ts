d8c412887919d2b283230c7c3165614aedd15173
Kristiyan Tachev <kristiqn.tachev@gmail.com>
2022-03-26 20:27:30 +0200

    fix(custom-attributes): added registry as default

--- packages/modifiers/src/angular/index.ts
+++ packages/modifiers/src/angular/index.ts
@@ -1,3 +1,5 @@
 import { IfOperator } from './if';
+import { NgFor } from './ng-for';
+import { Registry } from './registry';
 
-export const AngularLayout = [IfOperator];
+export const AngularLayout = [Registry, IfOperator, NgFor];
