460194e89ca62296918ae9054aa812d5768e873f
Kristiyan Tachev <kristiqn.tachev@gmail.com>
2024-05-19 22:19:53 +0300

    feat(docs): updated documentation [ci-skip]

--- documentation/docusaurus.config.ts
+++ documentation/docusaurus.config.ts
@@ -8,15 +8,15 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://r-html.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/rhtml/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'r-html', // Usually your GitHub org/user name.
+  projectName: 'rhtml', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -72,7 +72,7 @@ const config: Config = {
         },
         { to: '/blog', label: 'Blog', position: 'left' },
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/r-html/rhtml',
           label: 'GitHub',
           position: 'right',
         },
@@ -121,7 +121,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      // copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
--- documentation/src/components/HomepageFeatures/index.tsx
+++ documentation/src/components/HomepageFeatures/index.tsx
@@ -15,32 +15,22 @@ const FeatureList: FeatureItem[] = [
   {
     title: 'Easy to Use',
     Svg: mountain,
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
+    description: <>Generate scalable applications with ease</>,
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Reusable and maintainable',
     Svg: tree,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Write logic which can be reused and maintained for the future
+        generations
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Inspired by @Angular',
     Svg: undraw,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
+    description: <>Use a dependency injection which you already know!</>,
   },
 ];
 
